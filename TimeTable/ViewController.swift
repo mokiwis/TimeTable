a5a7d3274e59b582f193410bddbe40cfc3f56ccf
Moisés Cervantes <cervantes.moises@me.com>
2015-08-21 14:34:30 -0500

    Initial Commit

---
+++ TimeTable/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TimeTable
+//
+//  Created by Moisés Cervantes on 21/08/15.
+//  Copyright (c) 2015 Moisés Cervantes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
