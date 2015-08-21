d6d562f04ff8a295a28eee9b05f714f983fb308f
Moisés Cervantes <cervantes.moises@me.com>
2015-08-21 14:52:14 -0500

    Slider

--- TimeTable/ViewController.swift
+++ TimeTable/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var sliderValue: UISlider!
+    
+    @IBAction func sliderMoved(sender: AnyObject) {
+        
+        println(sliderValue)
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
